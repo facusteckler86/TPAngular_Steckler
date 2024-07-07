67cd29f0b20a2fc2fd00bc9ac61701a5ea55b49e
facusteckler86 <facundomsteckler@gmail.com>
2024-07-07 11:41:33 -0300

    al fin arregle la terminal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatInputModule} from '@angular/material/input';
-import { DashboardModule } from './features/dashboard/dashboard.module';
+//import { DashboardModule } from './features/dashboard/dashboard.module';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,DashboardModule,MatInputModule],
+  imports: [RouterOutlet,DashboardComponent,MatInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -4,18 +4,27 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { DashboardComponent } from './dashboard.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 
 
 @NgModule({
-  declarations: [],
-  exports: [DashboardComponent],
+  declarations:[],
+  exports: [DashboardModule],
   imports: [
     CommonModule,
     DashboardRoutingModule,
     MatSidenavModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule
   ]
 })
 export class DashboardModule {
